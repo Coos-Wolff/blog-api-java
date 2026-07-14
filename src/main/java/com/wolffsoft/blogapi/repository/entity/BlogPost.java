a6563b2ce88894532327f34c08924ec48f22b339
Coos Wolff <coos.wolff@outlook.com>
2026-07-14 13:40:58 +0200

    Add User and BlogPost entities, initial schema migration, and local Postgres compose

---
+++ src/main/java/com/wolffsoft/blogapi/repository/entity/BlogPost.java
@@ -0,0 +1,68 @@
+package com.wolffsoft.blogapi.repository.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Generated;
+import org.hibernate.generator.EventType;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "blog_post")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BlogPost {
+
+    @Id
+    @Generated(event = EventType.INSERT)
+    @ColumnDefault("gen_random_uuid()")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "title", nullable = false, length = 250, unique = true)
+    private String title;
+
+    @Column(name = "subtitle", nullable = false, length = 250)
+    private String subtitle;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "body", columnDefinition = "text", nullable = false)
+    private String body;
+
+    @Column(name = "img_url", nullable = false, length = 250)
+    private String imgUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof BlogPost blogPost)) {
+            return false;
+        }
+        return id != null && id.equals(blogPost.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/java/com/wolffsoft/blogapi/repository/entity/User.java
@@ -0,0 +1,80 @@
+package com.wolffsoft.blogapi.repository.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.generator.EventType;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @Generated(event = EventType.INSERT)
+    @ColumnDefault("gen_random_uuid()")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "email", nullable = false, unique = true, length = 254)
+    private String email;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Generated(event = EventType.INSERT)
+    @ColumnDefault("false")
+    @Column(name = "is_admin", nullable = false)
+    private Boolean isAdmin;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @Setter(AccessLevel.NONE)
+    private Set<BlogPost> blogPosts = new HashSet<>();
+
+    public void addBlogPost(BlogPost blogPost) {
+        blogPosts.add(blogPost);
+        blogPost.setAuthor(this);
+    }
+
+    public void removeBlogPost(BlogPost blogPost) {
+        blogPosts.remove(blogPost);
+        blogPost.setAuthor(null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User user)) {
+            return false;
+        }
+        return id != null && id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
